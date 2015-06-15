c423a51e94048e8625818a73d3ffb828ad9867cb
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-06-15 13:58:35 -0700

    seed file for politicians worksa - b

--- db/migrate/20150615180731_create_politicians.rb
+++ db/migrate/20150615180731_create_politicians.rb
@@ -3,6 +3,9 @@ class CreatePoliticians < ActiveRecord::Migration
     create_table :politicians do |t|
       t.string :name
       t.string :office
+      t.string :cid
+      t.string :party
+      t.string :state
       t.timestamps null: false
     end
   end
