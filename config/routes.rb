faaedd294cb1e5b0b3268d87c8212e7061d187f5
skwongg <silaskwong1@gmail.com>
2015-06-17 22:10:22 -0700

    database hooked up to the view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'contributors#index'
+    resources :contributors do
+    end
 
+    resources :contributions do
+    end
+
+    resources :politicians do
+    end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
